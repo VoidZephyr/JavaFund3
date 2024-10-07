f8c7d60b111fe698045466a99ada936c3c3655ab
VoidZephyr <jking21412@gmail.com>
2024-10-07 10:59:33 -0700

    new push

--- practice.js
+++ practice.js
@@ -1,2 +1,35 @@
-test push 
+// const updateAge = (object, age) => {
+//     object.age = age
+//     return object
+//   }
+//   const updateAge = (object, age) => {
+//     return { ...object, age };
+//   };
+  
+
+//----------------------------
+//   Given the following function signature, use what you've learned to allow printArgs() to return an array with all individual arguments that were passed to the function (an undefined number).
+
+//let printArgs = (/**/) => {
+//     //YOUR CODE HERE
+//   };
+
+// let printArgs = (...args) => {
+//     return args;
+//   };
+//   console.log(printArgs(1, 2, 3, 4));
+//------------------------------------
+
+// const address = (addressParts) => (
+//     `${addressParts.street}, ${addressParts.city}, ${addressParts.state} ${addressParts.zip}`
+// );
+
+// const address = ({ street, city, state, zip }) => (
+//     `${street}, ${city}, ${state} ${zip}`
+//   );
+
+
+  //--------------------------------
+
+  
 
